2445beae716175d587df8fa8e14e235cc1775fa8
JAGIUpo <148251101+JAGIUpo@users.noreply.github.com>
2023-12-22 11:03:48 +0100

    validacion telefono
    añadida la validacion de phone

--- inmobiliaria_upo/models/persona.py
+++ inmobiliaria_upo/models/persona.py
@@ -8,7 +8,7 @@ class persona(models.Model):
 
     category_ids = fields.Many2many(relation=False)#No es ninguna variable de nuestro programa, no se utiliza simplemente necesito poner su relation como falsa para poder heredar de hr.employee correctamente
     name = fields.Char(string="DNI", size=10)
-    phone = fields.Char(string="Teléfono")
+    phone = fields.Char(string="Teléfono", size=9)
     work_email = fields.Char(string="Correo Electrónico")
     
     nombre = fields.Char(string="Nombre", required=True)
@@ -20,3 +20,15 @@ class persona(models.Model):
     def _validar_edad(self):
         if self.edad < 0:
             raise ValidationError("La edad no puede ser negativa")
+        
+    @api.constrains('phone')
+    def _validar_phone(self):
+        try:
+            int(self.phone)
+            if len(self.phone) < 9:
+                raise ValidationError("El número de teléfono tiene que tener 9 dígitos")
+            elif int(self.phone) < 0:
+                raise ValidationError("El número de teléfono no puede ser negativo")
+        except ValueError:
+            raise ValidationError("El número de teléfono tiene que ser un número")
+        
